ad3ff6614147a57d2767381ec150e6e0c34a128a
Sávio Muniz <savio.muniz@ccc.ufcg.edu.br>
2016-12-12 09:36:54 -0300

    Pre processamento completo

--- Parte3/parte3.R
+++ Parte3/parte3.R
@@ -1,3 +1,4 @@
+####################### PRE-PROCESSAMENTO #########################
 library(readr)
 library(dplyr)
 
@@ -16,3 +17,26 @@ graduados.clean <- graduados %>%
 
 summary(graduados.clean)
 View(graduados.clean)
+
+graduados.cra <- graduados.clean %>%
+  group_by(ALU_NOVAMATRICULA) %>%
+  mutate(cra.contrib = MAT_MEDIA_FINAL*CREDITOS) %>%
+  summarise(cra = sum(cra.contrib)/sum(CREDITOS))
+
+head(graduados.cra)
+
+library(reshape2)
+
+graduados.model.input <- graduados.clean %>%
+  group_by(ALU_NOVAMATRICULA,DISCIPLINA)  %>%
+  filter(MAT_MEDIA_FINAL == max(MAT_MEDIA_FINAL)) %>%
+  ungroup() %>%
+  select(ALU_NOVAMATRICULA,DISCIPLINA,MAT_MEDIA_FINAL) %>% 
+  mutate(DISCIPLINA = as.factor(gsub(" ",".",DISCIPLINA))) %>%
+  dcast(ALU_NOVAMATRICULA ~ DISCIPLINA, mean) %>%
+  merge(graduados.cra)
+
+head(graduados.model.input)
+View(graduados.model.input)
+
+############################################################################
